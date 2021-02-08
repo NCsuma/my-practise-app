a8aba8492c1cde5ba7b5531bdc62c9ff04b87069
NCsuma <nallansuma@gmail.com>
2021-02-08 22:10:53 +0530

    Made some changes

---
+++ BirdItem.py
@@ -0,0 +1,7 @@
+class Item:
+    name = None
+    image_name = None
+
+    def __init__(self,name,bird_image_name):
+        self.name = name
+        self.bird_image_name = bird_image_name
--- FlowerItem.py
+++
@@ -1,9 +0,0 @@
-class FlowerItem:
-    id = None
-    name = None
-    image_name = None
-
-    def __init__(self,id,name,image_name):
-        self.id = id
-        self.name = name
-        self.image_name = image_name
